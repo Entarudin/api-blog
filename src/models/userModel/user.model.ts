ca7c1fe5395549c9c348b214f6c89f226f2956c0
Entarudin <riazanov@sfedu.ru>
2023-01-30 17:59:45 +0300

    update relations in user model

--- src/models/userModel/user.model.ts
+++ src/models/userModel/user.model.ts
@@ -1,6 +1,5 @@
 import { prop } from '@typegoose/typegoose';
 import { TimeStamps, Base } from '@typegoose/typegoose/lib/defaultClasses';
-import { TokenPairModel } from '../tokenPairModel';
 import { RoleModel } from '../roleModel';
 
 export interface UserModel extends Base {}
@@ -11,9 +10,6 @@ export class UserModel extends TimeStamps {
   @prop()
   public passwordHash: string;
 
-  @prop({ type: () => RoleModel })
+  @prop({ type: () => [RoleModel] })
   public roles: RoleModel[];
-
-  @prop({ type: () => TokenPairModel })
-  public tokens: TokenPairModel[];
 }
