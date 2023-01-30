9bf5567d83d42c5b8db25bb6c425a0e168124d93
Entarudin <riazanov@sfedu.ru>
2023-01-30 18:01:36 +0300

    added in root dependecied modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { ControllersModule } from './controllers/controllers.module';
+import { ServicesModule } from './services/services.module';
+import { FiltersModule } from './fillters/fillters.module';
 
 @Module({
   imports: [
     DatabaseModule,
+    ControllersModule,
+    ServicesModule,
+    FiltersModule,
     ConfigModule.forRoot({
       isGlobal: true,
     }),
