bfb6c9c0d10834a97eb18dfd2a6089355fd13821
Entarudin <riazanov@sfedu.ru>
2023-01-30 17:58:03 +0300

    added user exeptions

--- src/exeptions/user-exeptions/index.ts
+++ src/exeptions/user-exeptions/index.ts
@@ -1,7 +1,13 @@
-import { NotFoundException } from '@nestjs/common';
+import { BadRequestException, NotFoundException } from '@nestjs/common';
 
 export class UserByIdNotFoundExeption extends NotFoundException {
   constructor(id: string) {
     super(`User with id=${id} not found`);
+  }
+}
+
+export class UserAlreadyExistByEmailExeption extends BadRequestException {
+  constructor() {
+    super('User with this email already exists');
   }
 }
