be7b80203c95314d9849a898500675d87d76775b
Entarudin <riazanov@sfedu.ru>
2023-03-05 13:03:05 +0300

    model Irepositories  extends Ibase repository

--- src/repositories/interfaces/post-repository.interface.ts
+++ src/repositories/interfaces/post-repository.interface.ts
@@ -1,12 +1,9 @@
 import { CreatePostDto, UpdatePostDto } from 'src/dtos/post-dto';
 import { PostModel } from '../../models/postModel';
 import { IFindPostOptions } from 'src/options/find-post.options.interface';
+import { IBaseCRUDRepository } from './base-crud-repository.interface';
 
-export interface IPostRepository {
-  create(dto: CreatePostDto): Promise<PostModel>;
-  update(id: string, dto: UpdatePostDto): Promise<PostModel>;
-  delete(id: string): Promise<void>;
-  findAll(): Promise<PostModel[]>;
+export interface IPostRepository
+  extends IBaseCRUDRepository<PostModel, CreatePostDto, UpdatePostDto> {
   findByOptions(options: IFindPostOptions): Promise<PostModel[]>;
-  findById(id: string): Promise<PostModel>;
 }
--- src/repositories/interfaces/user-repository.interface.ts
+++ src/repositories/interfaces/user-repository.interface.ts
@@ -2,13 +2,14 @@ import { UpdateUserDto, CreateUserDto } from 'src/dtos/user-dto';
 import { UserModel } from '../../models/userModel';
 import { IFindUserOptions } from 'src/options/find-user.options.interface';
 import { RoleModel } from 'src/models/roleModel';
+import { IBaseCRUDRepository } from './base-crud-repository.interface';
 
-export interface IUserRepository {
+export interface IUserRepository
+  extends Omit<
+    IBaseCRUDRepository<UserModel, CreateUserDto, UpdateUserDto>,
+    'create'
+  > {
   create(dto: CreateUserDto, role: RoleModel): Promise<UserModel>;
-  update(id: string, dto: UpdateUserDto): Promise<UserModel>;
-  delete(id: string): Promise<void>;
-  findAll(): Promise<UserModel[]>;
-  findById(id: string): Promise<UserModel>;
   findByEmail(email: string): Promise<UserModel | undefined>;
   findByOptions(options: IFindUserOptions): Promise<UserModel[]>;
 }
