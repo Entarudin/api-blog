a392dce4702d39c0b584f192f9d411fc670a084f
Entarudin <riazanov@sfedu.ru>
2023-03-14 00:03:04 +0300

    added pagination in find by options

--- src/repositories/interfaces/post-repository.interface.ts
+++ src/repositories/interfaces/post-repository.interface.ts
@@ -1,9 +1,9 @@
-import { CreatePostDto, UpdatePostDto } from 'src/dtos/post-dto';
+import { CreatePostDto, UpdatePostDto, FindPostsDto } from 'src/dtos/post-dto';
 import { PostModel } from '../../models/postModel';
-import { IFindPostOptions } from 'src/options/find-post.options.interface';
 import { IBaseCRUDRepository } from './base-crud-repository.interface';
+import { Pagination } from 'src/common/typegoose';
 
 export interface IPostRepository
   extends IBaseCRUDRepository<PostModel, CreatePostDto, UpdatePostDto> {
-  findByOptions(options: IFindPostOptions): Promise<PostModel[]>;
+  findByOptions<T extends Pagination<PostModel>>(dto: FindPostsDto): Promise<T>;
 }
